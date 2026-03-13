a00b6cb4d08e410b600e96acfc4488af8e36f398
IsakBH <isak@brunhenriksen.net>
2026-03-13 11:15:37 +0100

    add styling.css and <head>

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html>
     <head>
-
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <link rel="stylesheet" href="styles/styling.css" />
+        <link rel="icon" href="assets/ordlogo.png" />
     </head>
 
     <body>

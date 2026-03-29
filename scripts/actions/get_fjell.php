e341380704249a300f53dce69e4bfb0997d2ed7e
isakbh <isak@brunhenriksen.net>
2026-03-29 14:46:06 +0200

    fikset database include path til å bruke __DIR__ i get_fjell.php

--- scripts/actions/get_fjell.php
+++ scripts/actions/get_fjell.php
@@ -1,6 +1,6 @@
 <?php
 // kobler til database
-require_once "../database.php";
+require_once dirname(__DIR__, 2) . "/config/database.php";
 
 // henter alt fra fjell tabellen
 $sql = "select fjell.navn, fjell.beskrivelse, fjell.hoyde, fjell.fotografi, omrade.navn as omradenavn from fjell join omrade on omrade.id = fjell.region order by fjell.hoyde desc;";

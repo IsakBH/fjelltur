7566c651aa0308dcadd6632cb3f8eeb5f92c697a
isakbh <isak@brunhenriksen.net>
2026-03-29 17:04:54 +0200

    get_fjellturer.phps sql statementen henter nå faktisk data fra fjellturer tabellen

---
+++ scripts/actions/get_fjellturer.php
@@ -0,0 +1,15 @@
+<?php
+// kobler til database
+require_once dirname(__DIR__, 2) . "/config/database.php";
+
+// henter alt fra fjell tabellen
+$sql = "
+select fjelltur.navn, fjelltur.beskrivelse, fjelltur.dato, person.brukernavn, fjell.navn
+from fjelltur
+join person on fjelltur.person = person.id
+join fjell on fjelltur.fjell = fjell.id;
+";
+$stmt = $mysqli->prepare($sql);
+$stmt->execute();
+$result = $stmt->get_result();
+echo json_encode($result->fetch_all(MYSQLI_ASSOC));

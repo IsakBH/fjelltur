2c2adfca34630645e9288b6243ed3ec10481f7ea
isakbh <isak@brunhenriksen.net>
2026-03-31 00:03:15 +0200

    flyttet login.php og google-callback.php til en egen 'auth' mappe, lagt til logout.php i auth mappen og flyttet privacy policy til storage/policies/privacypolicy.txt

--- pages/auth/login.php
+++ pages/auth/login.php
@@ -1,18 +1,18 @@
 <?php
-require_once dirname(__DIR__, 2) . "/fjelltur/config/database.php";
+require_once dirname(__DIR__, 2) . "/config/database.php";
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
         <title>Login</title>
-        <link rel="stylesheet" href="../styles/oauth.css">
+        <link rel="stylesheet" href="../../styles/oauth.css">
     </head>
     <body>
         <h2>Logg inn med Google</h2>
         <p>For å bruke dette programmet må du logge inn med Google.</p>
         <i>Hvorfor må jeg logge inn med Google?</i>
-        <li>Du må logge inn med Google fordi dette programmet henter data fra Fitbits APIer, som bruker Google OAuth2.0 for autentisering. Les <a href="privacypolicy.txt">personvernsreglene her</a></li>
+        <li>Du må logge inn med Google fordi dette programmet henter data fra Fitbits APIer, som bruker Google OAuth2.0 for autentisering. Les <a href="/fjelltur/storage/policies/privacypolicy.txt">personvernsreglene her</a></li>
 
         <a href="google-callback.php">
             <button class="gsi-material-button">

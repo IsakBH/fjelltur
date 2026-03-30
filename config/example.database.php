54cb2af1fa11f876587f8daafe770390fff3361b
isakbh <isak@brunhenriksen.net>
2026-03-30 19:24:46 +0200

    begynt på google oauth2.0 login

--- config/example.database.php
+++ config/example.database.php
@@ -9,3 +9,7 @@ $mysqli = new mysqli($host, $username, $password, $database);
 if ($mysqli->connect_error) {
     die("Tilkobling mislykket: " . $mysqli->connect_error);
 }
+
+define('GOOGLE_CLIENT_ID', '');
+define('GOOGLE_CLIENT_SECRET', '');
+define('GOOGLE_REDIRECT_URI', 'http://localhost/fjelltur/pages/google-callback.php');

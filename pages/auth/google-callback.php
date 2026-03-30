1c8a5a7ab0207bacc10571a61f1fd6a1c56bbcc5
isakbh <isak@brunhenriksen.net>
2026-03-31 01:48:17 +0200

    flyttet alt inn i en try block for å ha mulighet for error handling, fikset problem med case sensitivity i de to sql statementsene,

--- pages/auth/google-callback.php
+++ pages/auth/google-callback.php
@@ -17,47 +17,57 @@ $client->addScope("profile");
 
 // hvis $_GET['code'] er satt, fetch en access token med fetchAccessTokenWithAuthCode() og lagre den i session
 if(isset($_GET['code'])) {
-    $token = $client->fetchAccessTokenWithAuthCode($_GET['code'], $_SESSION['code_verifier']);
-    $_SESSION['id_token_token'] = $token;
+    try {
+        $token = $client->fetchAccessTokenWithAuthCode($_GET['code'], $_SESSION['code_verifier']);
+        $_SESSION['id_token_token'] = $token;
 
-    // hvis tokenen er satt
-    if (!empty($_SESSION['id_token_token']) && isset($_SESSION['id_token_token']['id_token'])) {
-        $token_data = $client->verifyIdToken($token['id_token']);
-        $client->setAccessToken($_SESSION['id_token_token']);
-        print_r(array_keys($token_data));
-        echo "<br> <br>";
-        echo $token_data['name'];
-        echo "<img src='{$token_data['picture']}'>";
-        $username = $token_data['name'];
-        $email = $token_data['email'];
-        $profilepicture = $token_data['picture'];
-        $oauthprovider = "Google";
-        $oauth_uid = $token_data['sub'];
+        // hvis tokenen er satt
+        if (!empty($_SESSION['id_token_token']) && isset($_SESSION['id_token_token']['id_token'])) {
+            $token_data = $client->verifyIdToken($token['id_token']);
+            $client->setAccessToken($_SESSION['id_token_token']);
+            print_r(array_keys($token_data));
+            echo "<br> <br>";
+            echo $token_data['name'];
+            echo "<img src='{$token_data['picture']}'>";
+            $username = $token_data['name'];
+            $email = $token_data['email'];
+            $profilepicture = $token_data['picture'];
+            $oauthprovider = "google";
+            $oauth_uid = $token_data['sub'];
 
-        $sql = "SELECT * FROM person WHERE oauth_uid = ? AND oauth_provider = 'google';";
-        $stmt = $mysqli->prepare($sql);
-        $stmt->bind_param("s", $token_data['sub']);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        // hvis resultatet fra sql queryen var tom, så betyr jo det at brukeren ikke finnes og at den må opprettes i databasen
-        if($result->num_rows == 0){
-            $sql = "INSERT INTO person (brukernavn, epost, profilbilde, oauth_provider, oauth_uid) VALUES (?, ?, ?, ?, ?);";
+            $sql = "SELECT * FROM person WHERE oauth_uid = ? AND oauth_provider = 'google';";
             $stmt = $mysqli->prepare($sql);
-            $stmt->bind_param("sssss", $username, $email, $profilepicture, $oauthprovider, $oauth_uid);
+            $stmt->bind_param("s", $token_data['sub']);
             $stmt->execute();
+            $result = $stmt->get_result();
+
+            // hvis resultatet fra sql queryen var tom, så betyr jo det at brukeren ikke finnes og at den må opprettes i databasen
+            if($result->num_rows == 0){
+                $sql = "INSERT INTO person (brukernavn, epost, profilbilde, oauth_provider, oauth_uid) VALUES (?, ?, ?, ?, ?);";
+                $stmt = $mysqli->prepare($sql);
+                $stmt->bind_param("sssss", $username, $email, $profilepicture, $oauthprovider, $oauth_uid);
+                $stmt->execute();
+            }
+
+            $_SESSION['user'] = [
+                'name' => $username,
+                'email' => $email,
+                'profilepicture' => $profilepicture,
+            ];
+
+            header('Location: ../../index.php');
         }
+        else { // hvis id_token_token IKKE er satt,
+            $_SESSION['code_verifier'] = $client->getOAuth2Service()->generateCodeVerifier();
+            $authUrl = $client->createAuthUrl();
+        }
+    } catch (Exception $e) {
+        session_destroy();
+        exit("Authentication failed: " . $e->getMessage());
     }
-
-    else { // hvis id_token_token IKKE er satt,
-        $_SESSION['code_verifier'] = $client->getOAuth2Service()->generateCodeVerifier();
-        $authUrl = $client->createAuthUrl();
-    }
-
-    /*header('Location: ../fjelltur.php');
-    exit;*/
 
 } else {
+    $_SESSION['code_verifier'] = $client->getOAuth2Service()->generateCodeVerifier();
     $auth_url = $client->createAuthUrl();
     header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
     exit;

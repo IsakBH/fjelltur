ff1408b507e0073148849bc6b165379be6d2592d
isakbh <isak@brunhenriksen.net>
2026-04-03 17:12:45 +0200

    nå skal alt i registrer_ny_tur funke. den tar filen du laster opp, gir den et nytt navn med egen naming scheme og flytter den til /fjelltur/storage/images/thumbnails

--- scripts/actions/registrer_ny_tur.php
+++ scripts/actions/registrer_ny_tur.php
@@ -1,28 +1,21 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $fjelltur_navn = $_POST['fjelltur-navn'];
+    $fjelltur_navn_trimmed = preg_replace('/\s+/', '-', $fjelltur_navn);
     $fjelltur_beskrivelse = $_POST['fjelltur-beskrivelse'];
     $fjelltur_dato = $_POST['fjelltur-dato'];
+    $fjelltur_thumbnail = $_FILES['fjelltur-thumbnail'];
+
+    // thumbnail
+    $fjelltur_thumbnail_filnavn = `{$fjelltur_navn_trimmed}{$fjelltur_dato}`;
 
     $allowed = ['jpg', 'jpeg', 'png']; // filtypene bruker for lov til å laste opp
     $filename = $_FILES['fjelltur-thumbnail']['name'];
-    $ext = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
+    $file_extension = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
 
-    if (in_array($ext, $allowed)) {
+    if (in_array($file_extension, $allowed)) {
         // lag random filnavn og last bildet opp til uploads
-        $new_filename = uniqid() . '.' . $ext;
-        move_uploaded_file($_FILES['new_profile_picture']['tmp_name'], '../uploads/' . $new_filename);
-
-        // oppdater profilbilde i databasen
-        $sql = "UPDATE users SET profile_picture = ? WHERE id = ?";
-        $stmt = $mysqli->prepare($sql);
-        $stmt->bind_param("si", $new_filename, $_SESSION['user_id']);
-
-        if ($stmt->execute()) {
-            $_SESSION['profile_picture'] = $new_filename;
-            $success = "Oppdatering av profilbilde vellykket";
-        } else {
-            $error = "Oppdatering av profilbilde mislykket";
-        }
+        $new_filename = `$fjelltur_thumbnail_filnavn.$file_extension`;
+        move_uploaded_file($_FILES['new_profile_picture']['tmp_name'], `../../storage/images/thumbnails/$new_filename`);
     }
 }

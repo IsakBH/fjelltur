a1a769839688e6c2441b8428dd802c4f2765b6b4
isakbh <isak@brunhenriksen.net>
2026-04-21 23:41:22 +0200

    fjernet all innhold i slett_fjelltur.php og slett_fjelltur.js. bruk html skjema istedet.

--- scripts/js/slett_fjelltur.js
+++ scripts/js/slett_fjelltur.js
@@ -2,21 +2,5 @@ const slett_fjelltur_dialog = document.getElementById('slett-fjelltur-dialog');
 const slett_fjelltur_knapp = document.getElementById('slett-fjelltur-knapp');
 
 function slett_fjelltur(fjelltur_id) {
-    console.log("Bruker vil slette fjelltur " + fjelltur_id + "!");
-    slett_fjelltur_dialog.showModal();
-        fetch('/scripts/actions/slett_fjelltur.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                id: documentId
-            })
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                slett_fjelltur_dialog.close();
-            }
-        });
+
 }

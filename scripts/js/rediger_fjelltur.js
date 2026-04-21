488d6eefc10a711e71f3e9a324cc594cbffcdff0
isakbh <isak@brunhenriksen.net>
2026-04-21 21:27:56 +0200

    begynt på issue #20

--- scripts/js/rediger_fjelltur.js
+++ scripts/js/rediger_fjelltur.js
@@ -1,11 +1,24 @@
 const edit_fjelltur_dialog = document.getElementById('rediger-fjelltur-dialog');
-const edit_fjelltur_fjelltur_id = document.getElementById('rediger-fjelltur-skjema-tur-id');
+const rediger_fjelltur_fjelltur_id = document.getElementById('rediger-fjelltur-skjema-tur-id');
+const rediger_fjelltur_fjelltur_navn = document.getElementById('rediger-fjelltur-skjema-navn');
+const rediger_fjelltur_fjelltur_beskrivelse = document.getElementById('rediger-fjelltur-skjema-beskrivelse');
+const rediger_fjelltur_fjelltur_dato = document.getElementById('rediger-fjelltur-skjema-dato');
+const rediger_fjelltur_fjelltur_fjell = document.getElementById('rediger-fjelltur-skjema-fjell');
+
 
 function open_rediger_fjelltur(id, navn, beskrivelse, dato, thumbnail, fjell) {
     fjelltur_info = {id, navn, beskrivelse, dato, thumbnail, fjell}
     console.log(`Bruker har åpnet dialogen for å redigere en eksisterende fjelltur.`);
+    console.log(fjelltur_info);
     edit_fjelltur_dialog.showModal();
-    edit_fjelltur_fjelltur_id.value = id;
+
+    // setter skjema verdiene til eksisterende fjelltur data
+    rediger_fjelltur_fjelltur_id.value = id;
+    rediger_fjelltur_fjelltur_navn.value = navn;
+    rediger_fjelltur_fjelltur_beskrivelse.value = beskrivelse;
+    rediger_fjelltur_fjelltur_dato.value = dato;
+    rediger_fjelltur_fjelltur_fjell.value = fjell;
+
 }
 
 function close_fjelltur_edit() {

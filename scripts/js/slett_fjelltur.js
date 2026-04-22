d29519a41cce22d554c480e400178cfe0d0953e4
IsakBH <isak@brunhenriksen.net>
2026-04-22 08:38:35 +0200

    slett_fjelltur.js viser nå slett_fjelltur_dialog modalen og console logger

--- scripts/js/slett_fjelltur.js
+++ scripts/js/slett_fjelltur.js
@@ -2,5 +2,6 @@ const slett_fjelltur_dialog = document.getElementById('slett-fjelltur-dialog');
 const slett_fjelltur_knapp = document.getElementById('slett-fjelltur-knapp');
 
 function slett_fjelltur(fjelltur_id) {
-
+    console.log("Hei, dette er Isak Brun. Det virker som at brukeren har trykket på slett knappen på en fjelltur. :D");
+    slett_fjelltur_dialog.showModal();
 }
